0e7370620a0425dda5b1bc5da1e87d5fcfb3e1de
ahmedjhidri <167217614+ahmedjhidri@users.noreply.github.com>
2025-04-22 02:44:46 +0200

    Add files via upload

---
+++ theme-toggle.js
@@ -0,0 +1,13 @@
+if (localStorage.getItem('theme') === 'dark' || 
+    (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+} else {
+    document.documentElement.setAttribute('data-theme', 'light');
+}
+
+const toggleButton = document.getElementById('theme-toggle');
+toggleButton.addEventListener('click', () => {
+    const newTheme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+});
